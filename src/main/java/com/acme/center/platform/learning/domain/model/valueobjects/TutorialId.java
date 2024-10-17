5513de86fda77d9d394f6a533231b578033df7ec
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-16 20:56:24 -0500

    feat(learning-path): add learning path item value objects.

---
+++ src/main/java/com/acme/center/platform/learning/domain/model/valueobjects/TutorialId.java
@@ -0,0 +1,22 @@
+package com.acme.center.platform.learning.domain.model.valueobjects;
+
+import jakarta.persistence.Embeddable;
+
+
+/**
+ * TutorialId value object
+ * @summary
+ * This value object represents the unique identifier of a tutorial.
+ * The tutorialId must be greater than 0. It throws an IllegalArgumentException if the tutorialId is null or less than or equal to 0.
+ * @see IllegalArgumentException
+ * @since 1.0
+ */
+@Embeddable
+public record TutorialId(Long tutorialId) {
+
+    public TutorialId {
+        if (tutorialId == null || tutorialId <= 0) {
+            throw new IllegalArgumentException("TutorialId cannot be null or less than or equal to 0");
+        }
+    }
+}
