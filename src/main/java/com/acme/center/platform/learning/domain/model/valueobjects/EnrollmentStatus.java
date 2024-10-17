b67f9c12a68b40f57dc448d47ed21fb104c1eb42
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-16 20:55:33 -0500

    feat(enrollments): add enrollment value objects.

---
+++ src/main/java/com/acme/center/platform/learning/domain/model/valueobjects/EnrollmentStatus.java
@@ -0,0 +1,15 @@
+package com.acme.center.platform.learning.domain.model.valueobjects;
+
+/**
+ * Enumeration representing the enrollment status.
+ * @summary
+ * This enumeration is used to represent the enrollment status. It is used to indicate the status of the enrollment request.
+ * The possible values are REQUESTED, CONFIRMED, REJECTED, and CANCELLED.
+ * @since 1.0
+ */
+public enum EnrollmentStatus {
+    REQUESTED,
+    CONFIRMED,
+    REJECTED,
+    CANCELLED
+}
---
+++ src/main/java/com/acme/center/platform/learning/domain/model/valueobjects/ProgressStatus.java
@@ -0,0 +1,14 @@
+package com.acme.center.platform.learning.domain.model.valueobjects;
+
+/**
+ * Enumeration representing the progress status.
+ * @summary
+ * This enumeration is used to represent the progress status. It is used to indicate the status of the progress of student learning.
+ * The possible values are NOT_STARTED, STARTED, and COMPLETED.
+ * @since 1.0
+ */
+public enum ProgressStatus {
+    NOT_STARTED,
+    STARTED,
+    COMPLETED
+}
