09ea670fe446c6e45e085b139e3ad2fac13ffab1
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-20 13:35:09 -0500

    docs: add documentation comments to update student metrics on tutorial completed command.

--- src/main/java/com/acme/center/platform/learning/domain/model/commands/UpdateStudentMetricsOnTutorialCompletedCommand.java
+++ src/main/java/com/acme/center/platform/learning/domain/model/commands/UpdateStudentMetricsOnTutorialCompletedCommand.java
@@ -2,7 +2,18 @@ package com.acme.center.platform.learning.domain.model.commands;
 
 import com.acme.center.platform.learning.domain.model.valueobjects.AcmeStudentRecordId;
 
+/**
+ * Command to update student metrics on tutorial completed
+ * @param studentRecordId the student record id.
+ *                        Cannot be null or blank
+ */
 public record UpdateStudentMetricsOnTutorialCompletedCommand(AcmeStudentRecordId studentRecordId) {
+    /**
+     * Constructor
+     * @param studentRecordId the student record id.
+     *                        Cannot be null or blank
+     * @throws IllegalArgumentException if studentRecordId is null or blank
+     */
     public UpdateStudentMetricsOnTutorialCompletedCommand {
         if (studentRecordId == null || studentRecordId.studentRecordId() == null || studentRecordId.studentRecordId().isBlank()) {
             throw new IllegalArgumentException("studentRecordId cannot be null or blank");
