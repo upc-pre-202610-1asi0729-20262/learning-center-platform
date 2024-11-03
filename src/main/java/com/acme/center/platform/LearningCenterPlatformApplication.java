0709842cd834c551afd1261e39038e590694311e
Angel Velasquez <pcisavel@upc.edu.pe>
2024-11-02 21:41:48 -0500

    chore: add audit annotation.

--- src/main/java/com/acme/center/platform/LearningCenterPlatformApplication.java
+++ src/main/java/com/acme/center/platform/LearningCenterPlatformApplication.java
@@ -1,8 +1,11 @@
 package com.acme.center.platform;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class LearningCenterPlatformApplication {
 
