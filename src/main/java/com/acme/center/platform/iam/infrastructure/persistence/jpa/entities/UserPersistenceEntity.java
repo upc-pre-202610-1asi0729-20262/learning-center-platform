fb4d8aa4c48e401f97a114338a442da383282d44
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-30 01:20:11 -0500

    fix(persistence): remove cascading on user roles to prevent unintended updates.

--- src/main/java/com/acme/center/platform/iam/infrastructure/persistence/jpa/entities/UserPersistenceEntity.java
+++ src/main/java/com/acme/center/platform/iam/infrastructure/persistence/jpa/entities/UserPersistenceEntity.java
@@ -25,7 +25,7 @@ public class UserPersistenceEntity extends AuditableAbstractPersistenceEntity {
     @Column(name = "password", nullable = false, length = 120)
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
